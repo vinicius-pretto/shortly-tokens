ff092c221a8417ff3aed7f3a8182d1c82dae748a
Vinicius Pretto <vini.prettodasilva@gmail.com>
2020-05-29 17:54:32 -0300

    Add font-weight tokens

--- src/formatters/fontFormatter.js
+++ src/formatters/fontFormatter.js
@@ -1,18 +1,28 @@
 const _ = require("lodash");
 
+const getFontFamilyTokens = (frames) => {
+  const fontFrames = frames.find((frame) => frame.name === "font-family");
+  const fonts = fontFrames.children.filter((frame) => frame.type === "TEXT");
+  return fonts.map((font) => ({ [font.name]: font.style.fontFamily }));
+};
+
+const getFontWeightTokens = (frames) => {
+  const fontFrames = frames.find((frame) => frame.name === "font-weight");
+  const fonts = fontFrames.children.filter((frame) => frame.type === "TEXT");
+  return fonts.map((font) => ({ [font.name]: font.style.fontWeight }));
+};
+
 const getTokens = (data) => {
   let fontTokens = {};
   const frames = data.document.children[0].children;
-  const fontFrames = frames.find((frame) => frame.name === "font-family");
-  const fonts = fontFrames.children.filter((frame) => frame.type === "TEXT");
+  const fontFamilyTokens = getFontFamilyTokens(frames);
+  const fontWeightTokens = getFontWeightTokens(frames);
+  const fonts = [...fontFamilyTokens, ...fontWeightTokens];
 
-  fonts
-    .map((font) => ({ [font.name]: font.style.fontFamily }))
-    .forEach((font) => {
-      const key = Object.keys(font)[0];
-      _.set(fontTokens, key, font[key]);
-    });
-
+  fonts.forEach((font) => {
+    const key = Object.keys(font)[0];
+    _.set(fontTokens, key, font[key]);
+  });
   return fontTokens;
 };
 
