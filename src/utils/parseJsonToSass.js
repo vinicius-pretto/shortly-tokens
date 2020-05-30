cee409651a20bbdf6d8000d5a8dc013bead87f8c
Vinicius Pretto <vini.prettodasilva@gmail.com>
2020-05-30 08:24:39 -0300

    Fix

--- src/utils/parseJsonToSass.js
+++ src/utils/parseJsonToSass.js
@@ -1,8 +1,7 @@
 const _ = require("lodash");
-const tokens = require("../../tokens/shortly-tokens.json");
 
-const parseJsonToSass = () => {
-  return _.map(tokens, (value, key) => `$${key}: ${value};`).join("\n");
+const parseJsonToSass = (jsonTokens) => {
+  return _.map(jsonTokens, (value, key) => `$${key}: ${value};`).join("\n");
 };
 
 module.exports = parseJsonToSass;
